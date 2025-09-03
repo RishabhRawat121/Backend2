09b44461b5090fff9c0cfad8805ef787c7ea2d7e
RishabhRawat121 <rishabhrawat150leetcode@gmail.com>
2025-09-03 20:07:05 +0530

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -47,8 +47,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=False)
-    email = serializers.EmailField(required=False)
+    identifier = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
